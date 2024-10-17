061ea37bac7c09d947e34051771705cdda3d0d1b
HP <HP@LAPTOP-7L7IFFSP>
2024-10-17 18:57:33 +0300

    IAuthenticationService, ITokenService, IUserService have been added.

---
+++ AuthServer.Core/Configuration/Client.cs
@@ -0,0 +1,8 @@
+﻿namespace AuthServer.Core.Configuration;
+
+public class Client
+{
+    public Guid Id { get; set; }
+    public string Secret { get; set; }
+    public List<string> Audiences { get; set; }
+}
