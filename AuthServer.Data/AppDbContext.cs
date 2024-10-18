cfcfc166c41641a37d3b77fb6b5b07eeba084497
HP <HP@LAPTOP-7L7IFFSP>
2024-10-19 00:09:05 +0300

    DbContext has been added.

---
+++ AuthServer.Data/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using AuthServer.Core.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Data;
+
+public class AppDbContext : IdentityDbContext<UserApp, IdentityRole, string>
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {        
+    }
+
+    DbSet<Product> Products { get; set; }
+    DbSet<UserRefreshToken> UserRefreshTokens { get; set; }
+
+	protected override void OnModelCreating(ModelBuilder builder)
+	{
+        builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
+		base.OnModelCreating(builder);
+	}
+}
