7a70c65ff3fe6a4654e1a6a4d092d449c2c11eb2
HP <HP@LAPTOP-7L7IFFSP>
2024-10-19 14:29:57 +0300

    GenericRepository and UnitOfWork have been implemented.

---
+++ AuthServer.Data/Repositories/GenericRepository.cs
@@ -0,0 +1,53 @@
+﻿using AuthServer.Core.Repositories;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace AuthServer.Data.Repositories;
+
+public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+{
+	private readonly AppDbContext context;
+	private readonly DbSet<TEntity> dbSet;
+
+	public GenericRepository(AppDbContext context)
+	{
+		this.context = context;
+		dbSet = context.Set<TEntity>();
+	}
+
+	public async Task AddAsync(TEntity entity)
+	{
+		await dbSet.AddAsync(entity);
+	}
+
+	public async Task<IEnumerable<TEntity>> GetAllAsync()
+	{
+		return await dbSet.ToListAsync();
+	}
+
+	public async Task<TEntity> GetByIdAsync(string id)
+	{
+		var entity = await dbSet.FindAsync(Guid.Parse(id));
+		if (entity != null)
+		{
+			context.Entry(entity).State = EntityState.Detached;
+		}
+		return entity;
+	}
+
+	public void Remove(TEntity entity)
+	{
+		dbSet.Remove(entity); 
+	}
+
+	public TEntity Update(TEntity entity)
+	{
+		context.Entry(entity).State = EntityState.Modified;
+		return entity;
+	}
+
+	public IQueryable<TEntity> Where(Expression<Func<TEntity, bool>> predicate)
+	{
+		return dbSet.Where(predicate);
+	}
+}
---
+++ AuthServer.Data/UnitOfWork.cs
@@ -0,0 +1,24 @@
+﻿using AuthServer.Core.UnitOfWork;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServer.Data;
+
+public class UnitOfWork : IUnitOfWork
+{
+	private readonly DbContext context;
+
+    public UnitOfWork(AppDbContext context)
+    {
+        this.context = context;
+    }
+
+    public void Commit()
+	{
+		context.SaveChanges();
+	}
+
+	public async Task CommitAsync()
+	{
+		await context.SaveChangesAsync();
+	}
+}
